1aa0e91daec212ec540db81ec50f943996cb16d5
conf-author <68891925+conf-author@users.noreply.github.com>
2020-07-28 17:35:54 +0800

    Update Verifier.js

--- Client codes/Verifier.js
+++ Client codes/Verifier.js
@@ -2,7 +2,7 @@ var co = require('co');
 var fabricservice = require('./Service.js');
 var express = require('express');
 var app = express();
- 
+
 var channelid = "vegetableschannel";
 var cc_verify_arbitrary = "VerifyProofs_ArbitraryRange";
 var cc_verify_standard = "VerifyProofs_StandardRange";
@@ -12,21 +12,21 @@ The verifier perform the operation which invokes VerifyProofs_ArbitraryRange cha
 */
 app.get('/VerifyProofs_ArbitraryRange',function(req,res){
 
-    co(function *(){
+	co(function *(){
 
-        var keyid = req.query.keyid;
-        var minvalue = req.query.minvalue;
-        var maxvalue = req.query.maxvalue;
-		
-        var chaincodequeryresult = yield fabricservice.queryCc(cc_verify_arbitrary,"invoke",[keyid, minvalue, maxvalue]);
+		var keyid = req.query.keyid;
+		var minvalue = req.query.minvalue;
+		var maxvalue = req.query.maxvalue;
 
-        for(let i=0; i < chaincodequeryresult.length; i++){
-            res.send( chaincodequeryresult[i].toString('utf8'));
-        }
+		var chaincodequeryresult = yield fabricservice.queryCc(cc_verify_arbitrary,"invoke",[keyid, minvalue, maxvalue]);
 
-    }).catch((err) => {
-        res.send(err);
-    })
+		for(let i=0; i < chaincodequeryresult.length; i++){
+			res.send( chaincodequeryresult[i].toString('utf8'));
+		}
+
+	}).catch((err) => {
+		res.send(err);
+	})
 });
 
 /*
@@ -34,19 +34,19 @@ The verifier perform the operation which invokes VerifyProofs_StandardRange chai
 */
 app.get('/VerifyProofs_StandardRange',function(req,res){
 
-    co(function *(){
+	co(function *(){
 
-        var keyid = req.query.keyid;
+		var keyid = req.query.keyid;
 
-        var chaincodequeryresult = yield fabricservice.queryCc(cc_verify_standard,"invoke",[keyid]);
+		var chaincodequeryresult = yield fabricservice.queryCc(cc_verify_standard,"invoke",[keyid]);
 
-        for(let i=0; i < chaincodequeryresult.length; i++){
-            res.send( chaincodequeryresult[i].toString('utf8'));
-        }
+		for(let i=0; i < chaincodequeryresult.length; i++){
+			res.send( chaincodequeryresult[i].toString('utf8'));
+		}
 
-    }).catch((err) => {
-        res.send(err);
-    })
+	}).catch((err) => {
+		res.send(err);
+	})
 });
 
 /*
@@ -54,32 +54,33 @@ Accoding the version information of instantiated chaincode to verify chaincode i
 */
 app.get('/chaincodes',function(req,res){
 
-    co(function *(){
+	co(function *(){
 
-        var ccname = req.query.ccname;
-        var ccversion = req.query.ccversion;
-        console.info(ccname);
-        console.info(ccversion);
+		var ccname = req.query.ccname;
+		var ccversion = req.query.ccversion;
+		console.info(ccname);
+		console.info(ccversion);
 
-        var info = yield fabricservice.getInstantiatedChaincodes();
-        for (let i = 0; i < info.chaincodes.length; i++) {
+		var info = yield fabricservice.getInstantiatedChaincodes();
+		for (let i = 0; i < info.chaincodes.length; i++) {
 			//console.info('name: ' + info.chaincodes[i].name + ', version: ' +info.chaincodes[i].version + ', path: ' + info.chaincodes[i].path);
 			if ((info.chaincodes[i].name == ccname) && (info.chaincodes[i].version == ccversion)){
 				res.send("chaincode is true")
 			}
-        }
-        res.send("chaincode is false")
+		}
+		res.send("chaincode is false")
 
-    }).catch((err) => {
-        res.send(err);
-    })
+	}).catch((err) => {
+		res.send(err);
+	})
 });
 
 var server = app.listen(3003,function(){
-    var host = server.address().address;
-    var port = server.address().port;
+	
+	var host = server.address().address;
+	var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s',host,port);
+	console.log('Example app listening at http://%s:%s',host,port);
 })
 
 process.on('unhandledRejection',function(err){
